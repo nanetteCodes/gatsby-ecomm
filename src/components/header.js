a97b02c27a9cb5f32e3809925f55ff6e172a71f1
Nanette <njulius89@gmail.com>
2019-05-07 14:08:23 -0700

    header updates, style updates

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,8 @@ import PropTypes from "prop-types";
 import React from "react";
 import netlifyIdentity from "netlify-identity-widget";
 
-import gatsbyLogo from "../images/gatsby-icon.png";
+// import gatsbyLogo from "../images/gatsby-icon.png";
+import "./header.scss";
 
 const isActive = ({ isCurrent }) => {
   return { className: isCurrent ? "active" : "navlink" };
@@ -14,53 +15,49 @@ class Header extends React.Component {
   componentDidMount() {
     netlifyIdentity.init();
   }
+  // TODO: updated netlify identity UI
   render() {
     const { siteTitle } = this.props;
     return (
-      <header
-        style={{
-          background: `rebeccapurple`,
-          marginBottom: `1.45rem`
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `1.45rem 1.0875rem`
-          }}
-        >
+      <header>
+        <div className="headerContent">
           {/* title/ logo */}
-          <span style={{ display: "flex", alignItems: "center" }}>
-            <img
-              src={gatsbyLogo}
-              alt="gatsby garb logo"
-              style={{
-                borderRadius: "50%",
-                border: "3px solid orange",
-                margin: "0 5px",
-                width: "50px"
-              }}
-            />
-            <h1 style={{ margin: 0 }}>
+          {/* <span>
+            <img src={gatsbyLogo} alt="gatsby garb logo" />
+            <h1>
               <NavLink to="/">{siteTitle}</NavLink>
             </h1>
-          </span>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/blog">Blog</NavLink>
-          <NavLink to="/portfolio">Portfolio</NavLink>
-          <NavLink to="/cv">CV</NavLink>
+          </span> */}
+          <NavLink to="/" id="header-logo">
+            <span className="header-logo-box">
+              <span className="header-logo-initial">N</span>
+            </span>
+            <span className="header-logo-text" data-links>
+              {siteTitle}
+            </span>
+          </NavLink>
+
+          <NavLink to="/about" data-links>
+            About
+          </NavLink>
+          <NavLink to="/blog" data-links>
+            Blog
+          </NavLink>
+          <NavLink to="/portfolio" data-links>
+            Portfolio
+          </NavLink>
+          <NavLink to="/cv" data-links>
+            CV
+          </NavLink>
           {/* <NavLink to="/products">Store</NavLink> */}
 
-          <div data-netlify-identity-menu />
+          {/* <div data-netlify-identity-menu /> */}
         </div>
       </header>
     );
   }
 }
+
 Header.propTypes = {
   siteTitle: PropTypes.string
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -30,19 +30,8 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0
-          }}
-        >
+        <div id="layout">
           <main>{children}</main>
-          {/* <footer>
-            © {new Date().getFullYear()}, Built by{" "}
-            {data.site.siteMetadata.author}
-          </footer> */}
         </div>
         <Footer siteAuthor={data.site.siteMetadata.author} />
       </>
