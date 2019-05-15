e2b037d5ec672edcc6e6fdf7255402c3061940e9
Nanette <njulius89@gmail.com>
2019-05-14 19:11:45 -0700

    resume

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -3,14 +3,14 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-import Resume from "../../public/static/Nanette_Julius.pdf";
+import resume from "../../public/static/Nanette_Julius.pdf";
 
 const Cv = () => (
   <Layout>
     <SEO title="resume cv work experience" />
     <h1>Resume</h1>
     <div className="cv-title">
-      <a download="Nanette_Julius.pdf" className="btn-cv" href={Resume}>
+      <a download className="btn-cv" href={resume}>
         Download
       </a>
     </div>
