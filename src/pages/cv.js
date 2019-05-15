d8d9395f706ec4312ea107d20eb679e540d209d2
Nanette <njulius89@gmail.com>
2019-05-14 19:27:26 -0700

    resume bug fix

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,18 +1,21 @@
 import React from "react";
-//import { Link } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-//import resume from "../../public/static/Nanette_Julius.pdf";
+//import resume from 'Nanette_Julius.pdf'
 
 const Cv = () => (
   <Layout>
     <SEO title="resume cv work experience" />
     <h1>Resume</h1>
     <div className="cv-title">
-      {/* <a download="Nanette_Julius.pdf" className="btn-cv" href={resume}>
+      <a
+        download="Nanette_Julius.pdf"
+        className="btn-cv"
+        href="Nanette_Julius.pdf"
+      >
         Download
-      </a> */}
+      </a>
     </div>
     {/* <iframe style={{ width: "100%", height: "800px" }} src={Resume} /> */}
   </Layout>
