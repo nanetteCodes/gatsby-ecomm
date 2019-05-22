0b8e3ecfc63a762bea73ce82136a5026294a9a5f
Nanette <njulius89@gmail.com>
2019-05-22 14:32:52 -0700

    boxStyledBug

--- src/components/BoxStyled.js
+++ src/components/BoxStyled.js
@@ -1,14 +1,9 @@
 import React from "react";
-//import PropTypes from "prop-types";
 
 import "./boxStyled.scss";
 
 const BoxStyled = props => {
   return <span className="logo-box">{props.children}</span>;
 };
-
-// BoxStyled.propTypes = {
-//   children: PropTypes.node.isRequired
-// };
 
 export default BoxStyled;
