6f4009f7dffed7c067cb123504887767ac294907
Nanette <njulius89@gmail.com>
2019-05-22 15:47:44 -0700

    this is bs

--- src/components/linkBoxStyled.js
+++ src/components/linkBoxStyled.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "./linkBoxStyled.scss";
+// import "./linkBoxStyled.scss";
 
 const LinkBoxStyled = props => {
   return <span className="logo-box">{props.children}</span>;
