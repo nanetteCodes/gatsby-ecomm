8e9725390e22647a9b2c86f40a47ee810a69bde5
Nanette <njulius89@gmail.com>
2019-05-22 13:27:14 -0700

    check

---
+++ src/portfolioData/index.js
@@ -0,0 +1,17 @@
+export default [
+  { id: 1, title: "House in Hill" },
+  { id: 2, title: "Chi Town" },
+  { id: 3, title: "NYC Bird View" },
+  { id: 4, title: "Flowers" },
+  { id: 5, title: "Church Stairs" },
+  { id: 6, title: "Perisian Lyfe" },
+  { id: 7, title: "King Henrys Crib" },
+  { id: 8, title: "Irland Looking Beach Couple" },
+  { id: 9, title: "Snow and Grains" },
+  { id: 10, title: "Cool Blur" },
+  { id: 11, title: "Into the Amazon" },
+  { id: 12, title: "Brunching" },
+  { id: 13, title: "Plants" },
+  { id: 14, title: "Ice Ice Baby" },
+  { id: 15, title: "Cooking Stuff" }
+];
