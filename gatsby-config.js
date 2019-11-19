970679cb713cb163faad8628a535f63b5260c341
Nanette <njulius89@gmail.com>
2019-11-18 20:03:48 -0800

    updated resume

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/nanette-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/nanette-icon-v2.png`, // This path is relative to the root of the site.
         cache_busting_mode: `query`
       }
     },
--- src/components/leftSection.js
+++ src/components/leftSection.js
@@ -18,7 +18,7 @@ const LeftSection = props => {
         <h3>nanette.codes@gmail.com</h3>
         <a
           rel="noopener noreferrer"
-          href={withPrefix("Nanette_Julius.pdf")}
+          href={withPrefix("Nanette_Julius_11-18-2019.pdf")}
           target="_blank"
           className="logo-box"
         >
