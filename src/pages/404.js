45b5b1fb30f42056960c3c47cfe99b380f6def08
Nanette <njulius89@gmail.com>
2020-05-30 20:34:37 -0700

    404 update

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,36 @@
 import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <StaticQuery
+    query={graphql`
+      query {
+        sadFace: file(relativePath: { eq: "404.png" }) {
+          childImageSharp {
+            fluid(maxWidth: 600) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Layout>
+        <SEO title="404: Not found" />
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <div style={{ maxWidth: '450px' }}>
+          <Img fluid={data.sadFace.childImageSharp.fluid} alt="sad face 404" />
+        </div>
+
+      </Layout>
+    )}
+  />
+
 );
 
 export default NotFoundPage;
